80694f422bf2e58199b11f7e447233f6b85e90b7
Logan DiAdams <ltdiadams@mta.ca>
2019-11-27 19:02:34 -0400

    More errors more fixing...

--- csvparser/settings.py
+++ csvparser/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c#9mezh56gred6!k1l87g&v6vvc06ckr&#_+jd*90op=g+hb3w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ltdiadams.pythonanywhere.com']
+ALLOWED_HOSTS = ['ltdiadams.pythonanywhere.com', 'localhost']
 
 
 # Application definition
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
