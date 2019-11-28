7ef59fda3176074da1a3a58b6faeb87338c02eef
Logan DiAdams <ltdiadams@mta.ca>
2019-11-28 12:00:03 -0400

    Got working with new values.csv data

--- dataset/views.py
+++ dataset/views.py
@@ -22,11 +22,13 @@ dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 def fill_threshold(request):
     threshold = request.POST.get('threshold')
-    csv_path = os.path.join(dir_path, "media/stored.csv")
+    # csv_path = os.path.join(dir_path, "media/stored.csv")
+    csv_path = os.path.join(dir_path, "media/values.csv")
     df1 = pd.read_csv(csv_path)
     count = df1["threshold"].count()
     df1.loc[count ,"threshold"] = threshold
-    csv_path = os.path.join(dir_path, "media/stored.csv")
+    # csv_path = os.path.join(dir_path, "media/stored.csv")
+    csv_path = os.path.join(dir_path, "media/values.csv")
     df1.to_csv(csv_path, encoding='utf-8', index=False)
 
     return render(request, 'threshold.html')
@@ -34,20 +36,39 @@ def fill_threshold(request):
 def parse_data(request):
     result_dict = {}
     last_dict = {}
-    csv_path = os.path.join(dir_path, "media/stored.csv")
+    # csv_path = os.path.join(dir_path, "media/stored.csv")
+    csv_path = os.path.join(dir_path, "media/values.csv")
     df1 = pd.read_csv(csv_path)
     a = df1[-1:]
-    last_dict["moisture"] = int(a["moisture"])
-    last_dict["time"] = a["time"].values[0]
-    last_dict["pump"] = int(a["pump"])
-    last_dict["light"] = int(a["light"])
-    last_dict["temperature"] = int(a["temperature"])
+    # last_dict["moisture"] = int(a["moisture"])
+    # last_dict["time"] = a["time"].values[0]
+    # last_dict["pump"] = int(a["pump"])
+    # last_dict["light"] = int(a["light"])
+    # last_dict["temperature"] = int(a["temperature"])
+    #
+    # result_dict["moisture"] = list(df1["moisture"])
+    # result_dict["time"] = list(df1["time"])
+    # result_dict["pump"] = list(df1["pump"])
+    # result_dict["light"] = list(df1["light"])
+    # result_dict["temperature"] = list(df1["temperature"])
 
-    result_dict["moisture"] = list(df1["moisture"])
+    last_dict["time"] = a["time"].values[0]
+    # last_dict["pump"] = int(a["pump"])
+    last_dict["current_light"] = int(a["current_light"])
+    last_dict["current_temp"] = int(a["current_temp"])
+    last_dict["current_moist"] = int(a["current_moist"])
+
+    # result_dict["moisture"] = list(df1["moisture"])
+    # result_dict["time"] = list(df1["time"])
+    # result_dict["pump"] = list(df1["pump"])
+    # result_dict["light"] = list(df1["light"])
+    # result_dict["temperature"] = list(df1["temperature"])
+
     result_dict["time"] = list(df1["time"])
-    result_dict["pump"] = list(df1["pump"])
-    result_dict["light"] = list(df1["light"])
-    result_dict["temperature"] = list(df1["temperature"])
+    result_dict["current_light"] = list(df1["current_light"])
+    result_dict["current_temp"] = list(df1["current_temp"])
+    result_dict["current_moist"] = list(df1["current_moist"])
+
     return render(request, 'index.html', {'result_dict': result_dict, 'last_dict':last_dict})
 
 # def new_csv(request):
